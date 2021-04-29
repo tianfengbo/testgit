82474c4d96f51cd5f21b954fc422ed0ec02a97ed
tfb <2587118212@qq.com>
2021-04-29 09:29:43 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hahahah");
         System.out.println("bababababba");
+        System.out.println("ccccccccccc");
     }
 }
