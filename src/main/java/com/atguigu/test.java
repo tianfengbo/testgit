8ad84fec3341cf7d761797bc3b481e70d4a866a9
tfb <2587118212@qq.com>
2021-04-29 10:14:10 +0800

    push ssh commit

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("ssh test");
     }
 }
